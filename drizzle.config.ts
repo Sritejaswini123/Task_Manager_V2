99167a3d0fb74b7a3322a89c20e7bcc3d2d700f1
Sritejaswini123 <Sritejaswini1234@gmail.com>
2025-04-28 09:12:57 +0530

    Initial commit - Task Manager V2 setup

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+import * as fs from 'fs';  
+export default defineConfig({
+  out: './drizzle', 
+  schema: './src/db/*.ts',
+  dialect: 'postgresql', 
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync("./ca.pem").toString(),
+    },
+  },
+});
