061631218c5b7ea0a6611004900649b5873f6a12
Sritejaswini123 <Sritejaswini1234@gmail.com>
2025-04-28 15:59:02 +0530

    implemented jwt token - validations

---
+++ src/db/db.ts
@@ -0,0 +1,20 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as userSchema from './userSchema.js';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';  
+dotenv.config(); 
+const pool = new Pool({
+
+      host: process.env.DB_HOST!,
+      port: Number(process.env.DB_PORT!),
+      user: process.env.DB_USER!,
+      password: process.env.DB_PASSWORD!,
+      database: process.env.DB_NAME!,
+      ssl: {   
+        rejectUnauthorized: true, 
+        ca: fs.readFileSync("./ca.pem").toString(),
+      },
+});
+export const db = drizzle(pool, { schema: userSchema });
+// export const db = drizzle(pool, { schema: userSchema });
---
+++ src/utils/validation.ts
@@ -0,0 +1,18 @@
+// utils/validation.ts
+
+import { object, pipe, string, email, minLength } from "valibot";
+
+export const userValidationSchema = object({
+  email: pipe(
+    string(),
+    email() // built-in email validation
+  ),
+  password: pipe(
+    string(),
+    minLength(8, "Password must be at least 8 characters long")
+  ),
+  username: pipe(
+    string(),
+    minLength(3, "Username must be at least 3 characters long")
+  ),
+});
