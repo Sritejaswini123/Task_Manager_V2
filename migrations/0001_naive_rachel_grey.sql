d41605fc929f68d40adad610c1237875023b231f
Sritejaswini123 <sritejaswini1234@gmail.com>
2025-04-29 19:02:22 +0530

    resolve db errors

---
+++ migrations/0001_naive_rachel_grey.sql
@@ -0,0 +1,33 @@
+CREATE TABLE "userProjects" (
+	"project_id" serial PRIMARY KEY NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"description" text,
+	"created_by" integer,
+	"status" varchar(255) NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "userProjects_status_unique" UNIQUE("status")
+);
+--> statement-breakpoint
+CREATE TABLE "userTasks" (
+	"task_id" serial PRIMARY KEY NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"project_id" integer NOT NULL,
+	"description" text,
+	"deadline" timestamp,
+	"status" varchar(50) NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "password" SET DATA TYPE varchar;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "first_name" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "last_name" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "user_type" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "address" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "status" varchar(50) NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "updated_at" timestamp;--> statement-breakpoint
+ALTER TABLE "userProjects" ADD CONSTRAINT "userProjects_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "userTasks" ADD CONSTRAINT "userTasks_project_id_userProjects_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."userProjects"("project_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "username";--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "role";
--- src/db/schema/projectSchema.ts
+++ src/db/schema/projectSchema.ts
@@ -1,20 +1,15 @@
 
 import { pgTable, serial, text, timestamp, varchar,integer } from "drizzle-orm/pg-core";
+import { users } from "./userSchema";
 
 export const userProject=pgTable("userProjects",{
 
-    project_id:serial("project_id").primaryKey(),
-
+    id:serial("project_id").primaryKey(),
     title:varchar("title",{length:255}).notNull(),
-
     description: text("description"), 
-
-    created_by:integer(),
-
+    created_by:integer().references(()=>users.id),
     status:varchar("status", { length: 255 }).notNull().unique(),
-
     created_at:timestamp("created_at").defaultNow(),
-
     updated_at: timestamp("updated_at").defaultNow().notNull() 
 })
 
